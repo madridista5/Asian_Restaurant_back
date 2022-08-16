57304d6f8f9a099ef42b4784df995d17d4a81075
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-16 12:28:31 +0200

    Database was configured.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import dbConfiguration from './config/db.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfiguration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
