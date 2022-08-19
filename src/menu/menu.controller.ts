aef19ca1b2aabb205adbdeb655821cfedf0fde4f
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-19 13:58:08 +0200

    Method add was added to Menu Controller.

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post, UseGuards } from "@nestjs/common";
+import { MenuService } from "./menu.service";
+import { AddMenuDto } from "./dto/add-menu.dto";
+import { AdminGuard } from "../guards/admin.guard";
+import { AuthGuard } from "@nestjs/passport";
 
-@Controller('menu')
-export class MenuController {}
+@Controller('/menu')
+export class MenuController {
+  constructor(
+    @Inject(MenuService) private menuService: MenuService,
+  ) {}
+
+  @Post('/add')
+  @UseGuards(AdminGuard)
+  @UseGuards(AuthGuard("jwt"))
+  add(@Body() newDish: AddMenuDto): Promise<void> {
+    return this.menuService.add(newDish);
+  }
+
+}
--- src/menu/menu.service.ts
+++ src/menu/menu.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { AddMenuDto } from "./dto/add-menu.dto";
+import { DishInMenu } from "./dish-in-menu.entity";
 
 @Injectable()
-export class MenuService {}
+export class MenuService {
+
+  async add(newDish: AddMenuDto): Promise<void> {
+    const {category, name, price, description} = newDish;
+    const dishToAdd = new DishInMenu();
+    dishToAdd.category = category;
+    dishToAdd.name = name;
+    dishToAdd.price = price;
+    dishToAdd.description = description;
+    await dishToAdd.save();
+  }
+
+}
